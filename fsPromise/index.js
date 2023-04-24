7ff2b364ff1620f7facc4d8b656126504fd5852b
Osvaldo Ojeda <osvaldo.devmate@gmail.com>
2023-04-24 20:04:26 -0300

    clase 5 append file

--- fsPromise/index.js
+++ fsPromise/index.js
@@ -1,3 +1,5 @@
+const { kMaxLength } = require("buffer");
+const { error } = require("console");
 const fs = require("fs");
 
 // const read = (nombreArchivo) => {
@@ -11,10 +13,40 @@ const fs = require("fs");
 // ------------------
 const read = async (nombreArchivo) => {
   try {
-    const data= await fs.promises.readFile(`./${nombreArchivo}.txt`, "utf-8");
-    console.log(data)
+    const data = await fs.promises.readFile(`./${nombreArchivo}.txt`, "utf-8");
+    console.log(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
-read("data");
+//  read("data");
+
+// const append = (texto) => {
+//   return fs.promises
+//     .appendFile("./data.txt", `${texto}\n`)
+//     .then((data) => console.log(data))
+//     .catch((error) => console.log(error));
+// };
+
+const append = (texto) => {
+  return fs.promises.appendFile("./data.txt", `${texto}\n`);
+};
+
+append("clase n6")
+.then((data) => console.log(data))
+.catch((error) => console.log(error));
+// -----------------------------
+
+// const append2 = async (texto) => {
+//   try {
+//     const data = await fs.promises.appendFile("./data.txt", `${texto}\n`);
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+
+
+
+
